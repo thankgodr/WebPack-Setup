d18ee09b387a97efb21ded6c427ceafabf179e66
ThankGod Richard <madueketf@gmail.com>
2021-09-21 18:22:44 +0100

    Add static server and set mode to developmrnt

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   plugins: [
     new HtmlWebpackPlugin({
@@ -11,5 +12,16 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+  },
+  devServer: {
+    static: './dist',
   },
 };
